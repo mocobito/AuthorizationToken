90fdc93039c360990c2717ef738947801d6f40ad
mttai <mttai@cmcglobal.vn>
2024-02-15 08:59:04 +0700

    add validate and change method

--- src/main/java/com/ntuple/token/coupang/model/CoupangDto.java
+++ src/main/java/com/ntuple/token/coupang/model/CoupangDto.java
@@ -1,7 +1,6 @@
 package com.ntuple.token.coupang.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 public class CoupangDto {
